2d94277a1186e19dd273860fb469227aeb0da040
Stefan Midjich <swehack@gmail.com>
2016-06-29 12:24:30 +0200

    first commit

---
+++ boilerplate.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Boilerplate code to create CLI utilities
+
+from __future__ import print_function
+
+try:
+    from configparser import RawConfigParser
+except:
+    from ConfigParser import ConfigParser
+from argparse import ArgumentParser
+from pprint import pprint
+from sys import exit, stderr
+
+config = ConfigParser()
+config.readfp(open('boilerplate_defaults.cfg'))
+config.read(['/etc/boilerplate.cfg', './boilerplate_local.cfg'])
+
+parser = ArgumentParser(
+    description='Boilerplate code',
+    epilog='Example: ./boilerplate.py'
+)
+
+parser.add_argument(
+    '-v', '--verbose',
+    action='count',
+    default=False,
+    dest='verbose',
+    help='Verbose output, use more v\'s to increase level'
+)
+
+def main():
+    print('Boilerplate')
+
+if __name__ == '__main__':
+    main()
